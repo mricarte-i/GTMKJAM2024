467b26cc751eb51ed1e913005025d731c50fa5b5
mricarte <mricarte@itba.edu.ar>
2024-08-17 13:36:17 -0300

    camera and enemy changes

--- Mob.gd
+++ Mob.gd
@@ -1,18 +1,23 @@
-extends Camera2D
+extends CharacterBody2D
 
+@export var SPEED = 200.0
 @onready var target = %Player
-@export var weight = 1.1
-
 var last_pos: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	last_pos = target.global_position
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position = lerp(position, last_pos, weight)
+func _physics_process(delta: float) -> void:
+	#var direction = last_pos - position
+	var direction = position.direction_to(last_pos)
+	velocity = direction * SPEED
+	move_and_slide()
 	if not target == null:
 		last_pos = target.global_position
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
